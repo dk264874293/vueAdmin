7fce0785dd135d35e72c521da40d8a8b125ed8bf
peiliangWang <peiliang.wang@marcpoint.com>
2017-03-15 16:54:33 +0800

    页面部署

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import echarts from 'echarts'
+import router from './router'
+import store from './store'
 
+Vue.use(VueAxios, axios)
 Vue.use(ElementUI)
+Vue.use(Vuex)
+Vue.use(echarts)
 
 new Vue({
   el: '#app',
